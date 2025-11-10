1b720a73959f60bc625b24c5fc93ed2513739d8b
embiem <mbeierling@gmail.com>
2025-11-10 13:22:55 +0100

    Ensure db is initialized on server start

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"log"
+	"log/slog"
 	"net/http"
 
+	"github.com/embiem/book-rag/db"
+	"github.com/embiem/book-rag/handler"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+	initialize()
+	defer teardown()
+
 	r := chi.NewRouter()
 	r.Use(middleware.CleanPath)
 	r.Use(middleware.StripSlashes)
@@ -25,13 +32,27 @@ func main() {
 		w.Write([]byte("GET books, to see available books for querying"))
 	})
 
-	r.Post("/books", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("POST books to ingest a new book into vector db"))
-	})
+	r.Post("/books", handler.HandleIngestBook)
 
 	r.Get("/books/{bookID}", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("GET book with ID to query for snippets from the given book"))
 	})
 
+	slog.Info("Listening on :3000")
 	http.ListenAndServe(":3000", r)
+}
+
+func initialize() {
+	// TODO: ensure env vars are available
+
+	// Setup DB
+	if err := db.Init(); err != nil {
+		log.Fatalf("couldn't init db: %v", err)
+	}
+}
+
+func teardown() {
+	slog.Info("Teardown started...")
+	db.Teardown()
+	slog.Info("Teardown finished.")
 }
